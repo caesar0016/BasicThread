f1b7a75fea2a1428352ec513e12ff5620a852508
caesar0016 <dapitoncaesar26@gmail.com>
2023-10-20 19:09:15 +0800

    Updated Space

---
+++ MyThreadClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThread
+{
+    internal class MyThreadClass
+    {
+        public static void thread1()
+        {
+
+            for (int i = 0; i < 6; i++)
+            {
+
+                Thread thread1;
+                thread1 = Thread.CurrentThread;
+                Console.WriteLine("Name of the Thread: " + thread1.Name + " = " + i);
+                Thread.Sleep(1500);
+
+
+            }
+
+
+        }
+    }
+}
---
+++ thread_01.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BasicThread
+{
+    partial class thread_01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStart = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(100, 66);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(262, 24);
+            this.lblStart.TabIndex = 0;
+            this.lblStart.Text = "-- Before Starting Thread --";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(183, 109);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(483, 210);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblStart);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
